e2dac33732655271a27a2ccb5a08c2a0d7dd1cee
Peria <parkhyongsok@naver.com>
2018-09-07 11:47:37 +0900

    gridview completed
    count with decoding process needed

--- main.dart
+++ main.dart
@@ -1,8 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
+import 'dart:async';
+import 'dart:convert';
 import 'dart:math';
 import 'dart:typed_data';
+import 'package:http/http.dart' as http;
+
+
+Future<Postlist> fetchPost() async {
+  final response =
+//  await http.get('https://jsonplaceholder.typicode.com/posts/1');
+  await http.get('http://10.0.2.2:8000/API/articles/?format=json');
+  var completer = new Completer();
+
+  if (response.statusCode == 200) {
+    // If the call to the server was successful, parse the JSON
+//    print(json.decode(response.body));
+//    print(json.decode(response.body).length);
+//    for (var test in json.decode(response.body)) {
+//      print(test['title']);
+//    }
+
+    print(response.body);
+
+    return Postlist.fromJson(json.decode(response.body));
+  } else {
+    // If that call was not successful, throw an error.
+    throw Exception('Failed to load post');
+  }
+}
+
+class Postlist {
+  final List<Post> list;
+
+  Postlist({this.list});
+
+  factory Postlist.fromJson(json) {
+
+    List<Post> Posts = new List<Post>(json.length);
+
+    int counter = 0;
+    for (var index in json) {
+      Posts[counter] = Post(
+          upvote : index['upvote'],
+          title : index['title'],
+          image : index['image']
+      );
+      counter++;
+    }
+    return Postlist(
+        list: Posts
+    );
+  }
+}
+
+class Post {
+  final int upvote;
+  final String title;
+  final String image;
+
+  Post({this.upvote, this.title, this.image});
+
+  factory Post.fromJson(json) {
+    return Post(
+      upvote: json['upvote'],
+      title: json['title'],
+      image: json['image'],
+    );
+  }
+}
 
 
 void main() => runApp(new MyApp());
@@ -45,6 +112,51 @@ List<IntSize> _createSizes(int count) {
           (i) => new IntSize((rnd.nextInt(500) + 200), rnd.nextInt(800) + 200));
 }
 
+class SayProject extends StatefulWidget {
+  @override
+  _SayProjectState createState() => _SayProjectState();
+}
+
+class _SayProjectState extends State<SayProject> {
+  Postlist postList;
+
+  _SayProjectState() {
+    fetchPost().then((value) => setState(() {
+      postList = value;
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            title: Text("Say::Project"),
+            floating: true,
+            snap: true,
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.all(12.0),
+            sliver: SliverStaggeredGrid(
+              delegate: SliverVariableSizeChildBuilderDelegate(
+                      (context, index) => _Tile(index, postList.list[index])
+              ),
+              gridDelegate: SliverStaggeredGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 100.0,
+                  mainAxisSpacing: 0.0,
+                  crossAxisSpacing: 8.0,
+                  staggeredTileBuilder: (index) => new StaggeredTile.fit(2)
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
 class MyHomePage extends StatelessWidget {
   MyHomePage() : _sizes = _createSizes(_kItemCount).toList();
 
@@ -65,9 +177,21 @@ class MyHomePage extends StatelessWidget {
             padding: const EdgeInsets.all(12.0),
             sliver: SliverStaggeredGrid(
                 delegate: SliverVariableSizeChildBuilderDelegate(
-                        (context, index) => new _Tile(index, _sizes[index])
+                        (context, index) => FutureBuilder(
+                            future: fetchPost(),
+                            builder: (context, snapshot) {
+                              if (snapshot.hasData) {
+                                return new _Tile(index, snapshot.data.list[index]);
+                              } else if (snapshot.hasError) {
+                                return Text("${snapshot.error}");
+                              }
+
+                              // By default, show a loading spinner
+                              return CircularProgressIndicator();
+                            })
                 ),
                 gridDelegate: SliverStaggeredGridDelegateWithMaxCrossAxisExtent(
+                    staggeredTileCount: 2,
                     maxCrossAxisExtent: 100.0,
                     mainAxisSpacing: 0.0,
                     crossAxisSpacing: 8.0,
@@ -82,29 +206,64 @@ class MyHomePage extends StatelessWidget {
 }
 
 
-class Example08 extends StatelessWidget {
-  Example08() : _sizes = _createSizes(_kItemCount).toList();
+//class MyHomePage extends StatelessWidget {
+//  MyHomePage() : _sizes = _createSizes(_kItemCount).toList();
+//
+//  static const int _kItemCount = 1000;
+//  final List<IntSize> _sizes;
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      body: CustomScrollView(
+//        slivers: <Widget>[
+//          SliverAppBar(
+//            title: Text("Say::Project"),
+//            floating: true,
+//            snap: true,
+//          ),
+//          SliverPadding(
+//            padding: const EdgeInsets.all(12.0),
+//            sliver: SliverStaggeredGrid(
+//              delegate: SliverVariableSizeChildBuilderDelegate( (context, index) => new _Tile(index, snapshot.data.list[index]); ),
+//              gridDelegate: SliverStaggeredGridDelegateWithMaxCrossAxisExtent(
+//                  maxCrossAxisExtent: 100.0,
+//                  mainAxisSpacing: 0.0,
+//                  crossAxisSpacing: 8.0,
+//                  staggeredTileBuilder: (index) => new StaggeredTile.fit(2)
+//              ),
+//            ),
+//          )
+//        ],
+//      ),
+//    );
+//  }
+//}
 
-  static const int _kItemCount = 1000;
-  final List<IntSize> _sizes;
 
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('random dynamic tile sizes'),
-      ),
-      body: new StaggeredGridView.countBuilder(
-        primary: false,
-        crossAxisCount: 4,
-        mainAxisSpacing: 4.0,
-        crossAxisSpacing: 4.0,
-        itemBuilder: (context, index) => new _Tile(index, _sizes[index]),
-        staggeredTileBuilder: (index) => new StaggeredTile.fit(2),
-      ),
-    );
-  }
-}
+//class Example08 extends StatelessWidget {
+//  Example08() : _sizes = _createSizes(_kItemCount).toList();
+//
+//  static const int _kItemCount = 1000;
+//  final List<IntSize> _sizes;
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return new Scaffold(
+//      appBar: new AppBar(
+//        title: new Text('random dynamic tile sizes'),
+//      ),
+//      body: new StaggeredGridView.countBuilder(
+//        primary: false,
+//        crossAxisCount: 4,
+//        mainAxisSpacing: 4.0,
+//        crossAxisSpacing: 4.0,
+//        itemBuilder: (context, index) => new _Tile(index, _sizes[index]),
+//        staggeredTileBuilder: (index) => new StaggeredTile.fit(2),
+//      ),
+//    );
+//  }
+//}
 
 class IntSize {
   const IntSize(this.width, this.height);
@@ -116,60 +275,83 @@ class IntSize {
 class _Tile extends StatelessWidget {
   const _Tile(this.index, this.size);
 
-  final IntSize size;
+  final Post size;
   final int index;
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: <Widget>[
-        new Card(
-          child: new Column(
-            children: <Widget>[
-              new Stack(
-                children: <Widget>[
-                  //new Center(child: new CircularProgressIndicator()),
-                  new Center(
-                    child: new FadeInImage.memoryNetwork(
-                      placeholder: kTransparentImage,
-                      image: 'https://picsum.photos/${size.width}/${size.height}/',
+        children: <Widget>[
+          new Card(
+            child: new Column(
+              children: <Widget>[
+                new Stack(
+                  children: <Widget>[
+                    //new Center(child: new CircularProgressIndicator()),
+                    new Center(
+                      child: new FadeInImage.memoryNetwork(
+                        placeholder: kTransparentImage,
+                        image: size.image,
+                      ),
+//                      child: new Text(
+//                        size.image,
+//                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-        Container(
-          height: 30.0,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              IconTheme(
-                  data: IconThemeData(
-                      color: Colors.red,
-                      opacity: 0.7,
-                      size: 12.0
-                  ),
-                  child: Icon(Icons.favorite)
-              ),
-              SizedBox(width: 5.0,),
-              Text("$index", style: TextStyle(fontSize: 13.0, color: Colors.grey, fontWeight: FontWeight.bold),),
-              SizedBox(width: 10.0,),
-              IconTheme(
-                  data: IconThemeData(
-                      color: Colors.grey,
-                      opacity: 0.7,
-                      size: 12.0
-                  ),
-                  child: Icon(Icons.chat)
-              ),
-              SizedBox(width: 5.0,),
-              Text("$index", style: TextStyle(fontSize: 13.0, color: Colors.grey, fontWeight: FontWeight.bold),),
-            ],
+          Container(
+            height: 25.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                IconTheme(
+                    data: IconThemeData(
+                        color: Colors.red,
+                        opacity: 0.7,
+                        size: 12.0
+                    ),
+                    child: Icon(Icons.favorite)
+                ),
+                SizedBox(width: 5.0,),
+                Text("${size.upvote}", style: TextStyle(fontSize: 13.0, color: Colors.grey, fontWeight: FontWeight.bold),),
+                SizedBox(width: 10.0,),
+                IconTheme(
+                    data: IconThemeData(
+                        color: Colors.grey,
+                        opacity: 0.7,
+                        size: 12.0
+                    ),
+                    child: Icon(Icons.chat)
+                ),
+                SizedBox(width: 5.0,),
+                Text("$index", style: TextStyle(fontSize: 13.0, color: Colors.grey, fontWeight: FontWeight.bold),),
+              ],
+            ),
           ),
-        )
-      ]
+          Center(
+            child: Text(size.title),
+          )
+        ]
     );
   }
 }
+
+
+//Center(
+//child: FutureBuilder<Post>(
+//future: fetchPost(),
+//builder: (context, snapshot) {
+//if (snapshot.hasData) {
+//return Text(snapshot.data.title);
+//} else if (snapshot.hasError) {
+//return Text("${snapshot.error}");
+//}
+//
+//// By default, show a loading spinner
+//return CircularProgressIndicator();
+//},
+//),
+//),
